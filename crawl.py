313e05d0f2dbffc1e44cd070d2abd1df19333e4d
Arnab Mukherjee <mukherjeearnab905@gmail.com>
2021-01-08 14:52:14 +0530

    Added function to iterate through repositories

--- crawl.py
+++ crawl.py
@@ -1,11 +1,13 @@
 from github import Github
 
 
-def get_files(ACCESS_TOKEN, repository, file_ext):
+def get_repo_files(ACCESS_TOKEN, repository, file_ext=''):
     FILES = []
 
     # SET GitHub Personal Access Token
     g = Github(ACCESS_TOKEN)
+
+    repositories = g.search_repositories(query='language:python')
 
     # SET Repository e.g. mukherjeearnab/policing-network
     repo = g.get_repo(repository)
@@ -23,3 +25,23 @@ def get_files(ACCESS_TOKEN, repository, file_ext):
                 FILES.append(file_dict)
 
     return FILES
+
+
+def get_repo_language(ACCESS_TOKEN, repo_language, file_ext=''):
+
+    REPO_FILES = []
+
+    # SET GitHub Personal Access Token
+    g = Github(ACCESS_TOKEN)
+
+    # GET repositories with language
+    repositories = g.search_repositories(query=f'language:{repo_language}')
+
+    # ITERATE repositories
+    for repo in repositories:
+        FILES = get_repo_files(access_token=ACCESS_TOKEN,
+                               repository=repo, file_ext=file_ext)
+        REPO = {'repository': repo.name, 'files': FILES}
+        REPO_FILES.append(REPO)
+
+        return REPO_FILES
